6f45cdacad9553470605814b2a5927c70e692713
yzijun <yzijun@126.com>
2016-01-03 22:13:46 +0800

    文章内容中有上传图片时，需要缩放图片的大小

--- src/main/java/com/app/finder/service/ArticleService.java
+++ src/main/java/com/app/finder/service/ArticleService.java
@@ -23,7 +23,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.time.ZonedDateTime;
+import java.util.Base64;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -73,23 +76,23 @@ public class ArticleService {
         int picHeight = widthAndHeight[1];
         //根据上传图片的宽高决定是否要缩放图片
         int width = 800;
-        int height = 500;
+        int height = 400;
         /*
          * 重新缩放上传图片的情况
-         * 1.若图片宽比800大，高比500大，图片按比例缩小，宽为800或高为500
-         * 2.若图片宽比800大，高比500小，宽缩小到800，图片比例不变 
+         * 1.若图片宽比800大，高比400大，图片按比例缩小，宽为800或高为400
+         * 2.若图片宽比800大，高比400小，宽缩小到800，图片比例不变 
          */
         if ((picWidth > width && picHeight > height)
         		|| (picWidth > width && picHeight < height)) {
-        	//重新缩放上传图片的大小 800 * 500
+        	//重新缩放上传图片的大小 800 * 400
         	ByteArrayOutputStream out = new ByteArrayOutputStream();
         	ThumbnailsUtils.resetPicture(width, height, article.getFirstImg(), out);
         	article.setFirstImg(out.toByteArray());
         } 
         /*
          * 这两种方式不做处理保持图片原来大小
-         * 1.若图片横比800小，高比500小
-         * 2.若图片横比800小，高比500大
+         * 1.若图片横比800小，高比400小
+         * 2.若图片横比800小，高比400大
          */
         
         //过滤敏感词汇
@@ -97,6 +100,31 @@ public class ArticleService {
         	String repContent = article.getContent().replaceAll(f.getWord(), "**");
         	article.setContent(repContent);
         });
+        //文章内容中有上传图片时，需要缩放图片的大小
+        //正则查找base64编码后的字符串.+贪婪模式.+?是非贪婪模式
+        Pattern p = Pattern.compile(";base64,(.+?)\"");
+        Matcher m = p.matcher(article.getContent());
+        while (m.find()) {
+        	//取得正则匹配(.+?)的字符串
+        	String s = m.group(1);
+        	//Base64解码为字节数组
+        	byte[] decode = Base64.getDecoder().decode(s);
+	    	is = new ByteArrayInputStream(decode);
+	        widthAndHeight = ThumbnailsUtils.getWidthAndHeight(is);
+	        picWidth = widthAndHeight[0];
+	        picHeight = widthAndHeight[1];
+        	if ((picWidth > width && picHeight > height)
+            		|| (picWidth > width && picHeight < height)) {
+            	//重新缩放上传图片的大小 800 * 400
+            	ByteArrayOutputStream out = new ByteArrayOutputStream();
+            	ThumbnailsUtils.resetPicture(width, height, decode, out);
+            	//图片缩放后用Base64编码
+            	String encodeStr = Base64.getEncoder().encodeToString(out.toByteArray());
+            	//替换原来的字符串
+            	m.replaceAll(encodeStr);
+            } 
+        	
+        }
         
         
         Article result = articleRepository.save(article);
