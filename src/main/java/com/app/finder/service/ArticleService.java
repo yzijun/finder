4ea147b6a227b350bf3cfb3ff9a2359ba13a5d70
yzijun <yzijun@126.com>
2016-01-06 16:43:59 +0800

    注释服务端缩小上传图片的代码，改为localResizeIMG客户端浏览器缩小图片

--- src/main/java/com/app/finder/service/ArticleService.java
+++ src/main/java/com/app/finder/service/ArticleService.java
@@ -1,31 +1,11 @@
 package com.app.finder.service;
 
-import com.app.finder.common.util.ThumbnailsUtils;
-import com.app.finder.domain.Article;
-import com.app.finder.domain.User;
-import com.app.finder.repository.ArticleRepository;
-import com.app.finder.repository.ForbiddenWordRepository;
-import com.app.finder.repository.UserRepository;
-import com.app.finder.repository.search.ArticleSearchRepository;
-import com.app.finder.security.SecurityUtils;
-import com.google.common.io.Files;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.stereotype.Service;
-
-import javax.inject.Inject;
+import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
 
 import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -36,7 +16,23 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import static org.elasticsearch.index.query.QueryBuilders.*;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.app.finder.domain.Article;
+import com.app.finder.domain.User;
+import com.app.finder.repository.ArticleRepository;
+import com.app.finder.repository.ForbiddenWordRepository;
+import com.app.finder.repository.UserRepository;
+import com.app.finder.repository.search.ArticleSearchRepository;
+import com.app.finder.security.SecurityUtils;
+import com.google.common.io.Files;
 
 /**
  * Service Implementation for managing Article.
@@ -75,7 +71,8 @@ public class ArticleService {
         User u = new User();
         u.setId(user.getId());
         article.setUser(user);
-        //取得上传图片的宽和高
+        //注释服务端缩小上传图片的代码，改为localResizeIMG客户端浏览器缩小图片
+        /*//取得上传图片的宽和高
         InputStream is = new ByteArrayInputStream(article.getFirstImg());
         int[] widthAndHeight = ThumbnailsUtils.getWidthAndHeight(is);
         int picWidth = widthAndHeight[0];
@@ -83,11 +80,11 @@ public class ArticleService {
         //根据上传图片的宽高决定是否要缩放图片
         int width = 800;
         int height = 400;
-        /*
+        
          * 重新缩放上传图片的情况
          * 1.若图片宽比800大，高比400大，图片按比例缩小，宽为800或高为400
          * 2.若图片宽比800大，高比400小，宽缩小到800，图片比例不变 
-         */
+         
         if ((picWidth > width && picHeight > height)
         		|| (picWidth > width && picHeight < height)) {
         	//重新缩放上传图片的大小 800 * 400
@@ -95,11 +92,11 @@ public class ArticleService {
         	ThumbnailsUtils.resetPicture(width, height, article.getFirstImg(), out);
         	article.setFirstImg(out.toByteArray());
         } 
-        /*
+        
          * 这两种方式不做处理保持图片原来大小
          * 1.若图片横比800小，高比400小
          * 2.若图片横比800小，高比400大
-         */
+         
         //保存图片的路径
         List<String> urlPics = new ArrayList<>();
         //文章内容中有上传图片时，需要缩放图片的大小
@@ -150,8 +147,45 @@ public class ArticleService {
     				bos.write(decode);
     			}
             }
+        }*/
+        
+        //保存图片的路径
+        List<String> urlPics = new ArrayList<>();
+        //文章内容中有上传图片时，需要缩放图片的大小
+        //正则查找base64编码后的字符串(.+)贪婪模式(.+?)非贪婪模式
+        Pattern p = Pattern.compile("<img src=\"data:image/(.+?);base64,(.+?)\"");
+        Matcher m = p.matcher(article.getContent());
+        while (m.find()) {
+        	//取得图片类型(后缀名)
+        	String picType = m.group(1);
+        	//取得图片base64编码后的字符串
+        	String picEncode = m.group(2);
+        	//Base64解码为字节数组
+        	byte[] decode = Base64.getDecoder().decode(picEncode);
+	        //TODO 正式系统需要改路径
+	        String savePicPath = "D:/apachePic/";
+	        //取得当前日期作为文件夹
+	        String day = ZonedDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+			
+	        String fileName = day + "/" + System.currentTimeMillis() + "." + picType;
+	        //保存图片的完整路径和文件名
+	        savePicPath += fileName;
+	        
+	        File f = new File(savePicPath);
+	        //google IO 不存在时创建父文件夹
+	        Files.createParentDirs(f);
+	        
+	        //图片显示URL中的路径
+	        //TODO 正式系统需要改URL地址
+	        urlPics.add("http://localhost:8089/" + fileName);
+        	//保存缩小后的图片
+        	try (BufferedOutputStream bos = new BufferedOutputStream(
+        			new FileOutputStream(savePicPath))) {
+				bos.write(decode);
+			}
         }
-        //替换图片为保存指定路径后的url
+        
+        //替换图片为保存到Apache指定路径后的url
         urlPics.forEach(s->{
         	String replace = "<img src=\"" + s + "\">";
         	String con = article.getContent().replaceFirst("<img src=\"data:image.+?\">", replace);
