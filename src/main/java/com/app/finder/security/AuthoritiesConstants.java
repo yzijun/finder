0400f8fec8dbed28d5decdf092ebe2c7975b970d
yzijun <yzijun@126.com>
2016-01-08 15:36:02 +0800

    增加文章管理角色

--- src/main/java/com/app/finder/security/AuthoritiesConstants.java
+++ src/main/java/com/app/finder/security/AuthoritiesConstants.java
@@ -8,7 +8,9 @@ public final class AuthoritiesConstants {
     public static final String ADMIN = "ROLE_ADMIN";
 
     public static final String USER = "ROLE_USER";
-
+    //增加文章管理角色
+    public static final String ARTICLE_ADMIN = "ROLE_ARTICLE_ADMIN";
+    
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {
