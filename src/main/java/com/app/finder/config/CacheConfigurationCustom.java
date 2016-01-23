1a84cdfcc26d22c6af7a396af750c75b96d51cc1
yzijun <yzijun@126.com>
2016-01-23 12:02:30 +0800

    自定义缓存配置类，不使用只是查看参考用

--- src/main/java/com/app/finder/config/CacheConfigurationCustom.java
+++ src/main/java/com/app/finder/config/CacheConfigurationCustom.java
@@ -10,7 +10,7 @@ import org.springframework.core.io.ClassPathResource;
  * 自定义的配置类  2016-1-22
  * 作用是开启spring的注解缓存@Cacheable可用
  * 
- *
+ * 不使用只是查看参考用
  */
 //@Configuration
 //@EnableCaching
@@ -19,7 +19,7 @@ public class CacheConfigurationCustom {
 	// 缓存实现用的是ehcache 不可用出错信息如下 可能是hibernate已经使用重复了
 	// No CacheResolver specified, and no unique bean of type CacheManager found. Mark one as primary (or give it the name 'cacheManager') or declare a specific CacheManager to use, that serves as the default one.
 	// @Primary
-	@Bean
+	/*@Bean
 	public CacheManager getEhCacheManager(){
 	    return new EhCacheCacheManager(getEhCacheFactory().getObject());
 	}
@@ -29,7 +29,7 @@ public class CacheConfigurationCustom {
 		factoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
 		factoryBean.setShared(true);
 		return factoryBean;
-	}
+	}*/
 	
 	
 	//JDK Cache Manager
