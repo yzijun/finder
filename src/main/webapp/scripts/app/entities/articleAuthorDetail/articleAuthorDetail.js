6fda9365cbbd8b5e3457c045a0567cd1b509aecc
yzijun <yzijun@126.com>
2016-02-28 21:40:56 +0800

    增加作者文章详细页面

---
+++ src/main/webapp/scripts/app/entities/articleAuthorDetail/articleAuthorDetail.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('finderApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('articleAuthorDetail', {
+                parent: 'entity',
+                url: '/articleAuthorDetails',
+                data: {
+//                    authorities: ['ROLE_USER'],
+                	//不需要登录不设定角色就可以显示文章作者详细页面
+                	authorities: [],
+                    pageTitle: 'finderApp.articleCategory.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/articleAuthorDetail/articleAuthorDetail.html',
+                        controller: 'ArticleAuthorDetailController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('articleCategory');
+                        $translatePartialLoader.addPart('global');
+                        return $translate.refresh();
+                    }]
+                }
+            })
+    });
