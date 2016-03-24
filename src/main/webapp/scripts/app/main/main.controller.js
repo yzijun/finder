250d2340341d9eef5ebef5e8731f460223077e63
yzijun <yzijun@126.com>
2016-03-24 14:52:54 +0800

    增加判断是否用户登录

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -5,11 +5,13 @@ angular.module('finderApp')
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
-            
-            $http.get('api/home/articlesum').success(function (response) {
-            	// 用户文章数量
-                $scope.articlesum = response;
-            });
+            // 是登录用户的场合
+            if ($scope.isAuthenticated()) {
+            	$http.get('api/home/articlesum').success(function (response) {
+            		// 用户文章数量
+            		$scope.articlesum = response;
+            	});
+            }
         });
         // 幻灯片 间隔5秒显示
        /* $('.carousel').carousel({
