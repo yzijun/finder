c62ae5814640bb1df9fffc5e2f1f1a9f71f1e282
yzijun <yzijun@126.com>
2016-04-05 17:04:08 +0800

    重新设定文章类别

--- src/main/java/com/app/finder/domain/ArticleCategory.java
+++ src/main/java/com/app/finder/domain/ArticleCategory.java
@@ -1,15 +1,25 @@
 package com.app.finder.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
  * 文章类别类.
@@ -25,9 +35,13 @@ public class ArticleCategory implements Serializable {
 
     //类别名称
     @NotNull
-    @Size(max = 20)
-    @Column(name = "name", length = 20, nullable = false)
+    @Size(max = 50)
+    @Column(name = "name", length = 50, nullable = false)
     private String name;
+    
+    // 父类别ID
+    @JoinColumn(name = "parent_id")
+    private ArticleCategory parentCategory;
 
     @OneToMany(mappedBy = "articleCategory")
     @JsonIgnore
@@ -58,8 +72,16 @@ public class ArticleCategory implements Serializable {
     public void setArticles(Set<Article> articles) {
         this.articles = articles;
     }
+    
+    public ArticleCategory getParentCategory() {
+		return parentCategory;
+	}
 
-    @Override
+	public void setParentCategory(ArticleCategory parentCategory) {
+		this.parentCategory = parentCategory;
+	}
+
+	@Override
     public boolean equals(Object o) {
         if (this == o) {
             return true;
