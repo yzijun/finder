e139efe6e48050df639287a5a6dbd9a7424ea83b
yzijun <yzijun@126.com>
2016-04-28 09:33:48 +0800

    增加示例代码参考用

--- src/main/webapp/scripts/components/user/user.service.js
+++ src/main/webapp/scripts/components/user/user.service.js
@@ -4,6 +4,8 @@ angular.module('finderApp')
     .factory('User', function ($resource) {
         return $resource('api/users/:login', {}, {
                 'query': {method: 'GET', isArray: true},
+                // 示例代码参考用
+//                'thisWeek': { method: 'GET', isArray: false, url: 'api/points-this-week'},
                 'get': {
                     method: 'GET',
                     transformResponse: function (data) {
