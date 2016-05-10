9b9b0c369accc14407ab993ccbf085e571669aea
yzijun <yzijun@126.com>
2016-05-10 11:31:26 +0800

    删除不用的webapp/scripts/components/entities/articleCategory和articleReply目录

--- src/main/webapp/scripts/components/entities/articleCategory/articleCategory.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('finderApp')
-    .factory('ArticleCategorySearch', function ($resource) {
-        return $resource('api/_search/articleCategorys/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
--- src/main/webapp/scripts/components/entities/articleReply/articleReply.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('finderApp')
-    .factory('ArticleReplySearch', function ($resource) {
-        return $resource('api/_search/articleReplys/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
